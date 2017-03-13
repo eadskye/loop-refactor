807cc6aedfaa67194c1423eb4aaebfe032fe0812
Elizabeth Davis <eadskye@gmail.com>
2017-03-12 20:16:24 -0600

    higher order function problems

--- main.js
+++ main.js
@@ -12,50 +12,71 @@ Run the command 'mocha' to test. Tests check for expected output and absence of 
 
 module.exports = {
 
+  // sum: (arr, base) => {
+  //   arr.reduce(sum(arr, base) => {
+  //     return arr + base;
+  //   }, 0);
+
+
+    // for (var i = 0; i < arr.length; i++){
+    //   sum += arr[i];
+    // }
+    // return sum;
+
+
   sum: (arr, base) => {
-    let sum = base;
-    for (var i = 0; i < arr.length; i++){
-      sum += arr[i];
-    }
-    return sum;
-  },
+    return arr.reduce((arr, base)=> {
+      return arr + base;
+    }, base);
+
+},
 
   someObjsContainProp: (arr, prop) => {
-    for(var i = 0; i < arr.length; i++){
-      if(arr[i].hasOwnProperty(prop)){
-        return true;
-      }
-    }
-    return false;
+    return arr.some((array)=>{
+      return array.property(prop);
+    });
   },
 
+
   convertNameArrayToObject: (arr) => {
-    let nameObj = [];
-    for(var i = 0; i < arr.length; i++){
-      let obj = {};
-      obj.first = arr[i][0];
-      obj.last = arr[i][1];
-      nameObj.push(obj);
-    }
-    return nameObj;
+    // let nameObj = [];
+    // for(var i = 0; i < arr.length; i++){
+    //   let obj = {};
+    //   obj.first = arr[i][0];
+    //   obj.last = arr[i][1];
+    //   nameObj.push(obj);
+    // }
+    // return nameObj;
+
+    return arr.map(nameObj => {
+      return nameObj.first[0], nameObj.last[1]
+    });
+
   },
 
   objContainsProp: (arr, prop) => {
-    for (var i = 0; i < arr.length; i++){
-      if(!arr[i].hasOwnProperty(prop)){
-        return false;
-      }
-    }
-    return true;
+    // for (var i = 0; i < arr.length; i++){
+    //   if(!arr[i].hasOwnProperty(prop)){
+    //     return false;
+    //   }
+    // }
+    // return true;
+    return arr.every(i => {
+      return i.property(prop);
+    });
   },
 
   stringMatch: (arr, str) => {
-    let matches = [];
-    for(var i = 0; i < arr.length; i++){
-      if (arr[i].includes(str)){
-        matches.push(arr[i]);
-      }
-    }
-    return matches;
-  },
+  //   let matches = [];
+  //   for(var i = 0; i < arr.length; i++){
+  //     if (arr[i].includes(str)){
+  //       matches.push(arr[i]);
+  //     }
+  //   }
+  //   return matches;
+  // },
+return arr.filter(i => {
+  return (i.match(str));
+});
+}
 };
